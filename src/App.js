017c935fcfc246bd5d9a98aa194ba54fbbb789ee
elazsyahranie <elazaribrahims@gmail.com>
2021-09-15 16:22:11 +0700

    Added socket client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
+import { useState, useEffect } from "react";
 import PublicRoute from "./pages/helpers/PublicRoute";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Login from "./pages/auth/login/login";
 import Register from "./pages/auth/register/register";
+import ChatList from "./pages/chat-list/chat-list";
+
+import io from "socket.io-client";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const setUpSocket = () => {
+    const newSocket = io.connect("http://localhost:3007", {
+      path: "/backend3/socket.io",
+    });
+    newSocket.on("connect", () => {
+      console.log("Connected socket client!");
+    });
+    setSocket(newSocket);
+  };
+
+  useEffect(() => {
+    setUpSocket();
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Switch>
           <PublicRoute path="/login" exact component={Login} />
           <PublicRoute path="/register" exact component={Register} />
+          <PublicRoute path="/chat-list" exact component={ChatList} />
         </Switch>
       </Router>
     </Provider>
---
+++ src/pages/chat-list/chat-list.js
@@ -0,0 +1,5 @@
+function ChatList() {
+  return <h1>ChatList page!</h1>;
+}
+
+export default ChatList;
