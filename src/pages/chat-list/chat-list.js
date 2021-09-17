8d54f27e7d5defb84fe69a4865dffbb51855ce52
elazsyahranie <elazaribrahims@gmail.com>
2021-09-17 18:28:23 +0700

    Added chat message room (init)

--- src/pages/chat-list/chat-list.js
+++ src/pages/chat-list/chat-list.js
@@ -1,37 +1,66 @@
 import { Container, Row, Col, Form } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import style from "./chat-list.module.css";
 import { connect } from "react-redux";
 
 function ChatList(props) {
   const [chatMessage, setChatMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState("");
+
+  useEffect(() => {
+    if (props.socket) {
+      props.socket.on("chatMessage", (dataMessage) => {
+        setChatMessages({ ...chatMessage, dataMessage });
+      });
+    }
+  }, [props.socket, chatMessages]);
 
   const handleChatMessage = (event) => {
-    setChatMessage({ [event.target.name]: event.target.value });
+    setChatMessage(event.target.value);
   };
+
+  const { user_name } = props.auth.data;
 
   const submitChatMessage = (event) => {
     if (event.keyCode === 13) {
       event.preventDefault();
-      console.log(chatMessage);
+      console.log("Username:", user_name);
+      console.log("Send message:", chatMessage);
+      const setData = {
+        user_name,
+        chatMessage,
+      };
+      props.socket.emit("globalMessage", setData);
     }
   };
 
   return (
     <>
-      <Container fluid>
-        <Row>
+      <Container fluid className={style.wholeContainer}>
+        <Row className={style.wholeRow}>
           <Col lg={3} md={3} sm={12} xs={12}>
-            <h5>User One</h5>
+            <h5>{user_name}</h5>
             <h5>User Two</h5>
           </Col>
           <Col lg={9} md={9} sm={12} xs={12} className={style.chatRoomStyling}>
             <h5>Chat room!</h5>
-            <Form>
+            <div className={style.chatMessagesContainer}>
+              <p>
+                <span className="fw-bold">The User:</span> Hi! How are you?
+              </p>
+              <p>
+                <span className="fw-bold">The User:</span> Hi! How are you?
+              </p>
+              <p>
+                <span className="fw-bold">The User:</span> Hi! How are you?
+              </p>
+            </div>
+            <Form className={style.formChat}>
               <Form.Group>
                 <Form.Control
                   type="text"
-                  name="chatMessage"
+                  className={style.formChatControl}
+                  value={chatMessage}
                   onChange={(event) => handleChatMessage(event)}
                   onKeyDown={(event) => submitChatMessage(event)}
                 ></Form.Control>
