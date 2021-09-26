5278ec36eb79d61bfb340073aae487a398bbb795
elazsyahranie <elazaribrahims@gmail.com>
2021-09-27 05:32:49 +0700

    Added settings

--- src/pages/settings/settings.js
+++ src/pages/settings/settings.js
@@ -1,8 +1,53 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
 import { connect } from "react-redux";
+import style from "./settings.module.css";
+import noProfilePicture from "../components/img-not-found.png";
+import leftArrow from "../components/back.png";
 
 function Settings(props) {
-  console.log(props.auth.data.user_email);
-  return <h5>The setting page!</h5>;
+  console.log(props.auth.data);
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col lg={3} md={3} sm={12} xs={12}>
+            <div className="mt-5 d-flex justify-content-between">
+              <div>
+                <Image
+                  src={leftArrow}
+                  alt=""
+                  className={style.shownDot}
+                  fluid
+                />
+              </div>
+              <h6 className={`text-center fw-bold ${style.userEmailStyling}`}>
+                {props.auth.data.user_email}
+              </h6>
+              <Image src={leftArrow} alt="" className={style.hiddenDot} />
+            </div>
+            <div className="mt-5 d-flex justify-content-center">
+              <Image
+                src={noProfilePicture}
+                alt=""
+                className={style.profilePictureStyling}
+                fluid
+              />
+            </div>
+            <div className="mt-4">
+              <h6 className="text-center">{props.auth.data.user_name}</h6>
+            </div>
+          </Col>
+          <Col
+            lg={9}
+            md={9}
+            sm={12}
+            xs={12}
+            className={style.chatRoomStyling}
+          ></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 const mapStateToProps = (state) => ({
