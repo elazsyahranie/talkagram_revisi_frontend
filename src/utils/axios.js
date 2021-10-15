e0afbf8f81dd3178cfdffd1fd26c1941c62a5420
elazsyahranie <elazaribrahims@gmail.com>
2021-10-15 19:15:43 +0700

    Axios interceptor

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -30,10 +30,10 @@ axiosApiIntances.interceptors.response.use(
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     console.log(error.reponse);
-    // if (error.response.status === 403) {
-    //   alert("please login");
-    //   localStorage.clear();
-    // }
+    if (error.response.status === 403) {
+      localStorage.clear();
+      window.location.href = "login";
+    }
     return Promise.reject(error);
   }
 );
